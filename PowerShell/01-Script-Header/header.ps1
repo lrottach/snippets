dd41213224da8ac9cb37f76cf0f9ebf343ba8f17
Lukas Rottach <50323692+lrottach@users.noreply.github.com>
2021-09-28 23:06:41 +0200

    feat: added PowerShell script header template

---
+++ PowerShell/01-Script-Header/header.ps1
@@ -0,0 +1,38 @@
+<#
+.SYNOPSIS
+  <Overview of script>
+.DESCRIPTION
+  <Brief description of script>
+.INPUTS
+  <Inputs if any, otherwise state None>
+.OUTPUTS
+  <Outputs if any, otherwise state None - Example: Log file stored in C:\Windows\Temp\<name>.log>
+.NOTES
+  Version:        <1.0>
+  Author:         <Name>
+  Creation Date:  <Date>
+  Purpose/Change: Initial script development
+  
+.EXAMPLE
+  <Example goes here. Repeat this attribute for more than one example>
+#>
+
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+
+# Example 01: Set error action preferences
+# Example 02: Import Modules or reference other scripts
+
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
+
+# Example
+# Declare Variables in this area
+
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+
+# Example
+# Use this area for writing functions
+
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
+
+# Example
+# Use this area for executing functions, commands, ...
